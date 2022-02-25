006a01614bddd7a95f5151c7d3fa64569a2b1a99
zzawook <kjaehyeok21@gmail.com>
2022-02-25 15:04:55 +0800

    SignIn SignUp UI Update
    Updated to implement flex instead of absolute

--- src/routes/SignIn.tsx
+++ src/routes/SignIn.tsx
@@ -22,64 +22,81 @@ const height = window.innerHeight;
 const width = window.innerWidth;
 const margin = 15;
 let linkMouseEnter = false;
-
+const Screen = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: ${width}px;
+    height: ${height}px;
+`
 const Container = styled.div`
-    position: absolute;
-    left: 0%;
-    top: 0%;
     width: 40%;
     height: 100%;
     background-color: #0B121C;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+const Inner = styled.div`
+    width: 70%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
 `
 const Back = styled.button`
-    position: absolute;
-    left: 25%;
-    top: 10%;
+    :hover {
+        border: 2px solid white;
+    }
+
     width: 95px;
-    height: 41px;
-    border: 2px solid white;
+    height: 50px;
+    max-height: 60px;
+    border: 2px solid transparent;
     color: #969696;
     background-color: #0B121C;
     font-weight: 500;
-    padding-left: 30px;
     cursor: pointer;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`
+const BackText = styled.span`
+    height: 60px;
+    width: 30px;
+    line-height: 60px;
+    padding-top: 2px;
 `
 const Title = styled.span`
-    position: absolute;
-    left: 25%;
-    top: 70%;
-    width: 50%;
-    text-align: center;
+    width: 100%;
     font-weight: 700;
     font-family: var(--font-family-roboto);
     color: white;
     font-size: 40px;
+    text-align: center;
     z-index: 1;
 `
 const Explanation = styled.span`
-    position: absolute;
-    left: 25%;
-    width: 50%;
+    margin-top: 20px;
+    width: 100%;
     text-align: center;
-    top: ${(height * 0.7) + 60}px;
     font-weight: 800;
     font-size: 13px;
     color: #808080;
     z-index: 1;
 `
 const Form = styled.form`
-    position: absolute;
-    right: 0%;
-    top: 0%;
     width: 60%;
     height: 100%;
     background-color: white;
     z-index: 0; 
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `
 const EmailInput = styled.input`
-    position: absolute;
-    left: 20%;
-    top: ${(height * 0.1) + (10 * margin)}px;
     border: none;
     border-bottom: 2px solid rgba(0, 0, 0, 0.3);
     width: 60%;
@@ -90,11 +107,9 @@ const EmailInput = styled.input`
     outline: none;
 `
 const PasswordInput = styled.input`
-    position: absolute;
-    left: 20%;
-    top: ${(height * 0.1) + (19 * margin)}px;
     border: none;
     border-bottom: 2px solid rgba(0, 0, 0, 0.3);
+    margin-top: 110px;
     width: 60%;
     height: 45px;
     font-family: var(--font-family-roboto);
@@ -103,9 +118,7 @@ const PasswordInput = styled.input`
     outline: none;
 `
 const SubmitButton = styled.input`
-    position: absolute;
-    right: 20%;
-    top: ${(height * 0.1) + (26 * margin)}px;
+    margin-top: 70px;
     width: 60%;
     height: 63px;
     border: none;
@@ -117,18 +130,14 @@ const SubmitButton = styled.input`
     cursor: pointer;
 `
 const ToSignUp = styled(Link)`
-    position: absolute;
-    left: 20%;
-    top: ${(height * 0.1) + (26 * margin) + 110}px;
+    margin-top: 20px;
     width: 60%;
     color: #808080;
     font-size: 12px;
     font-weight: 700;
 `
 const ToPassWord = styled.a`
-    position: absolute;
-    left: 20%;
-    top: ${(height * 0.1) + (26 * margin) + 80}px;
+    margin-top: 50px;
     width: 60%;
     color: #808080;
     font-size: 12px;
@@ -200,27 +209,25 @@ class SignIn extends React.Component<UserProps, UserObject> {
         window.history.back();
     }
     arrowStyle: CSS.Properties = {
-        position: "absolute",
-        left: "8px",
-        top: "8px",
-        height: '20px',
+        height: '40%',
+        marginRight: '5px',
     }
     imgStyle: CSS.Properties = {
-        position: 'absolute',
-        left: '20%',
-        width: '60%',
-        top: '20%'
+        width: '100%',
+        marginTop: '20px',
     }
 
     render() {
         return (
-            <>
+            <Screen>
                 <Container>
-                    <Back onClick={this.handleBackClick}><img style={this.arrowStyle} src='https://firebasestorage.googleapis.com/v0/b/nus-kusa-website.appspot.com/o/source%2FwhiteArrow.png?alt=media&token=efa6ec9b-d260-464e-bf3a-77a73193055f' />Back</Back>
-                    <Title>Log In</Title>
-                    <Explanation>환영합니다! 기존 이메일로 로그인 할 수 있습니다.</Explanation>
-
-                    <img src='https://firebasestorage.googleapis.com/v0/b/nus-kusa-website.appspot.com/o/source%2F8.png?alt=media&token=21e952d4-00f1-4a92-b0d2-28868e45e64f' style={this.imgStyle} />
+                    <Inner>
+                        <Back onClick={this.handleBackClick}><img style={this.arrowStyle} src='https://firebasestorage.googleapis.com/v0/b/nus-kusa-website.appspot.com/o/source%2FwhiteArrow.png?alt=media&token=efa6ec9b-d260-464e-bf3a-77a73193055f' /><BackText>Back</BackText></Back>
+                        <img src='https://firebasestorage.googleapis.com/v0/b/nus-kusa-website.appspot.com/o/source%2F8.png?alt=media&token=21e952d4-00f1-4a92-b0d2-28868e45e64f' style={this.imgStyle} />
+                        <Title>Log In</Title>
+                        <Explanation>환영합니다! 기존 이메일로 로그인 할 수 있습니다.</Explanation>
+                    </Inner> 
+                    
                 </Container>
                 <Form onSubmit={this.handleSubmit}>
                     <EmailInput
@@ -239,13 +246,13 @@ class SignIn extends React.Component<UserProps, UserObject> {
                         value={this.state.password}
                         onChange={this.handleChange}
                     />
-                    {/* Will be adding name, nickname, etc. */}
+                    <SubmitButton type="submit" value="Submit" />
                     {this.state.failed ? <FailMessage>Login failed. Please check your ID and Password.</FailMessage> : <></>}
                     <ToPassWord onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>Forgot Password?</ToPassWord>
                     <ToSignUp to='/signup' href="/#/signup" onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>Don't have an account? Click here to create an account!</ToSignUp>
-                    <SubmitButton type="submit" value="Submit" />
+                    
                 </Form>
-            </>
+            </Screen>
         )
     }
 }
--- src/routes/SignUp.tsx
+++ src/routes/SignUp.tsx
@@ -20,64 +20,82 @@ const height = window.innerHeight;
 const width = window.innerWidth;
 const margin = 15;
 let linkMouseEnter = false;
-
+const Screen = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: ${width}px;
+    height: ${height}px;
+`
 const Container = styled.div`
-    position: absolute;
-    left: 0%;
-    top: 0%;
     width: 40%;
     height: 100%;
     background-color: #0B121C;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+const Inner = styled.div`
+    width: 70%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
 `
 const Back = styled.button`
-    position: absolute;
-    left: 25%;
-    top: 10%;
+    :hover {
+        border: 2px solid white;
+    }
+
     width: 95px;
-    height: 41px;
-    border: 2px solid white;
+    height: 50px;
+    max-height: 60px;
+    border: 2px solid transparent;
     color: #969696;
     background-color: #0B121C;
     font-weight: 500;
-    padding-left: 30px;
     cursor: pointer;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`
+const BackText = styled.span`
+    height: 60px;
+    width: 30px;
+    line-height: 60px;
+    padding-top: 2px;
 `
 const Title = styled.span`
-    position: absolute;
-    left: 25%;
-    top: 70%;
-    width: 50%;
-    text-align: center;
+    width: 100%;
     font-weight: 700;
     font-family: var(--font-family-roboto);
     color: white;
     font-size: 40px;
+    text-align: center;
     z-index: 1;
 `
 const Explanation = styled.span`
-    position: absolute;
-    left: 25%;
-    width: 50%;
+    margin-top: 20px;
+    width: 100%;
     text-align: center;
-    top: ${(height * 0.7) + 60}px;
     font-weight: 800;
     font-size: 13px;
     color: #808080;
     z-index: 1;
 `
 const Form = styled.form`
-    position: absolute;
-    right: 0%;
-    top: 0%;
     width: 60%;
     height: 100%;
     background-color: white;
     z-index: 0; 
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `
 const NameInput = styled.input`
-    position: absolute;
-    left: 20%;
-    top: ${(height * 0.1) + (8 * margin)}px;
     border: none;
     border-bottom: 1px solid rgba(0, 0, 0, 0.3);
     width: 60%;
@@ -88,24 +106,20 @@ const NameInput = styled.input`
     outline: none;
 `
 const EmailInput = styled.input`
-    position: absolute;
-    left: 20%;
-    top: ${(height * 0.1) + (15 * margin)}px;
     border: none;
-    border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+    border-bottom: 2px solid rgba(0, 0, 0, 0.3);
     width: 60%;
     height: 45px;
     font-family: var(--font-family-roboto);
     font-weight: 700;
     font-size: 18px;
     outline: none;
+    margin-top: 90px;
 `
 const PasswordInput = styled.input`
-    position: absolute;
-    left: 20%;
-    top: ${(height * 0.1) + (22 * margin)}px;
     border: none;
-    border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+    border-bottom: 2px solid rgba(0, 0, 0, 0.3);
+    margin-top: 90px;
     width: 60%;
     height: 45px;
     font-family: var(--font-family-roboto);
@@ -114,27 +128,23 @@ const PasswordInput = styled.input`
     outline: none;
 `
 const SubmitButton = styled.input`
-    position: absolute;
-    right: 20%;
-    bottom: 20%;
+    margin-top: 70px;
     width: 60%;
     height: 63px;
     border: none;
     background-color: #BDA06D;
     color: white;
     font-weight: 700;
-    font-size: 22px;
+    font-size: 18px;
     font-family: var(--font-family-roboto);
     cursor: pointer;
 `
 const ToSignIn = styled(Link)`
-    position: absolute;
-    left: 20%;
-    bottom: 15%;
+    margin-top: 20px;
+    width: 60%;
     color: #808080;
     font-size: 12px;
-    font-weight: 600;
-    text-decoration: ${linkMouseEnter ? 'underline' : 'none'};
+    font-weight: 700;
 `
 class SignUp extends React.Component<UserProps, UserState> {
     constructor(props: UserProps) {
@@ -197,27 +207,26 @@ class SignUp extends React.Component<UserProps, UserState> {
         e.target.style.textDecoration = 'none';
     }
     arrowStyle: CSS.Properties = {
-        position: "absolute",
-        left: "8px",
-        top: "8px",
         height: '20px',
     }
     imgStyle: CSS.Properties = {
-        position: 'absolute',
-        left: '20%',
-        width: '60%',
-        top: '20%'
+        width: '100%',
+        marginTop: '20px',
     }
     render() {
         
 
         return (
-            <>
+            <Screen>
                 <Container>
-                    <Back onClick={this.handleBackClick}><img style={this.arrowStyle} src='https://firebasestorage.googleapis.com/v0/b/nus-kusa-website.appspot.com/o/source%2FwhiteArrow.png?alt=media&token=efa6ec9b-d260-464e-bf3a-77a73193055f'/>Back</Back>
-                    <Title>Sign Up to Join!</Title>
-                    <Explanation>NUS 한인회 사이트에 가입하시고 승인 받으시면 게시판의 더 많은 정보에 접속할 수 있습니다.</Explanation>
-                    <img src='https://firebasestorage.googleapis.com/v0/b/nus-kusa-website.appspot.com/o/source%2F8.png?alt=media&token=21e952d4-00f1-4a92-b0d2-28868e45e64f' style={this.imgStyle}/>
+                    <Inner>
+                        <Back onClick={this.handleBackClick}><img style={this.arrowStyle} src='https://firebasestorage.googleapis.com/v0/b/nus-kusa-website.appspot.com/o/source%2FwhiteArrow.png?alt=media&token=efa6ec9b-d260-464e-bf3a-77a73193055f'/><BackText>Back</BackText></Back>
+                        <img src='https://firebasestorage.googleapis.com/v0/b/nus-kusa-website.appspot.com/o/source%2F8.png?alt=media&token=21e952d4-00f1-4a92-b0d2-28868e45e64f' style={this.imgStyle}/>
+                        <Title>Sign Up to Join!</Title>
+                        <Explanation>NUS 한인회 사이트에 가입하시고 승인 받으시면 게시판의 더 많은 정보에 접속할 수 있습니다.</Explanation>
+                        
+                    </Inner>
+                    
                 </Container>
                 <Form onSubmit={this.handleSubmit}>
                     <NameInput
@@ -250,7 +259,7 @@ class SignUp extends React.Component<UserProps, UserState> {
                     <SubmitButton type="submit" value="Submit"/>
                     <ToSignIn to="/signin"  onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>Already have an account? Click here to Log in.</ToSignIn>
                 </Form>
-            </>
+            </Screen>
         )
     }
 }
