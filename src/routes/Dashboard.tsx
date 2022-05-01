0a2004f804d0cfb4a4048892acc127a3979a1abe
Yong Gi <rygrobin@gmail.com>
2022-05-01 13:00:11 +0800

    Setup Dashboard

---
+++ src/routes/Dashboard.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+type DashboardProps = {
+
+}
+
+type DashboardState = {
+
+}
+
+class Dashboard extends React.Component<DashboardProps, DashboardState> {
+    constructor(props: DashboardProps) {
+        super(props);
+    }
+
+    render = (): React.ReactNode => {
+        return (
+            <>
+                
+            </>
+        )
+    }
+}
